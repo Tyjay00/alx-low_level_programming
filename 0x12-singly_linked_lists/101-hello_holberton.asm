194b81f33cc8fcc373044b83af8788c2b4d62461
Tyrone Joel <tyronejoel04@gmail.com>
2023-04-28 13:26:52 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,12 @@
-global main: declares the symbol main as a global symbol
-extern printf: declares the printf function
-main: defines the entry point of the program
-mov edi, format: moves the address of the string
-xor eax, eax: clears the eax register
-call printf: calls the printf function with the arguments
-mov eax: sets the return value of the main function
-ret: returns control to the operating system, terminating the program
-format: db 'Hello, Holberton\n',0: this line defines the string
+extern    printf
+global   main
+
+main:
+mov   edi, format
+xor   eax, eax
+call  printf
+mov   eax, 0
+ret
+format: db `Hello, Holberton\n`,0
+
 
