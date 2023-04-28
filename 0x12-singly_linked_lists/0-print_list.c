8a1e9ca2af60d4ae3b803e3ea4061995c3b6bd05
Tyrone Joel <tyronejoel04@gmail.com>
2023-04-28 11:39:52 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_list - Prints the elements of a linked list
+ * @h: Print the pointer to the list_t pointer
  *
- * Return: the number of nodes printed
+ * Return: The number of nodes to be printed
  */
 size_t print_list(const list_t *h)
 {
@@ -23,3 +23,4 @@ size_t print_list(const list_t *h)
 
 	return (n);
 }
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,2 @@
 0x12. C - Singly linked lists
+C Algorithm Data structure
