ba39a232d82732c3be440d2af30d4afb59d17c15
Tyrone Joel <tyronejoel04@gmail.com>
2023-07-06 14:38:01 -0700

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int d;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (d = 0; b[d]; d++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[d] < '0' || b[d] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[d] - '0');
 	}
 
 	return (dec_val);
